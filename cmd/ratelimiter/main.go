11cfcec70ed5919e8afafbd3241ca54e1b05c72c
AndreyChufelin <chufelin.andrey@gmail.com>
2024-11-11 20:56:09 +0300

    update docker to load config

--- cmd/ratelimiter/main.go
+++ cmd/ratelimiter/main.go
@@ -21,7 +21,7 @@ import (
 var configFile string
 
 func init() {
-	flag.StringVar(&configFile, "config", "/etc/antibruteforce/config.toml", "Path to configuration file")
+	flag.StringVar(&configFile, "config", "configs/config.toml", "Path to configuration file")
 }
 
 func main() {
